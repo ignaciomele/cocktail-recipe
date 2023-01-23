25c1be0f25c81a639556095f088019253fe5ec6b
unknown <ignacio.mele@hotmail.com>
2023-01-23 20:23:05 +0100

    alert buttons welcome

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -3,21 +3,26 @@ import './welcome.css'
 import { useNavigate } from "react-router-dom";
 
 export default function Welcome() {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
+
+    const clickShowAlert = () => {
+        alert("We are working... We'll be back soon!")
+    }
+    
     return(
         <>
             <div className="div-welcome">
                 <h1>Welcome to Cocktail recipes</h1>
                 <div className="div-buttons-submit-welcome">
                     <div className="div-form-submit-button-welcome">
-                        <button onClick={() => navigate("/")} className="cssbuttons-io-button"><p>EXPLORE</p>
+                        <button onClick={clickShowAlert} className="cssbuttons-io-button"><p>EXPLORE</p>
                         <div className="icon">
                             <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path></svg>
                         </div>
                         </button>
                     </div>
                     <div className="div-form-submit-button-welcome">
-                        <button onClick={() => navigate("/")} type="submit" className="submit-btn-welcome">
+                        <button onClick={clickShowAlert} type="submit" className="submit-btn-welcome">
                             <span className="icon">
                             <p>+</p>
                             </span>
